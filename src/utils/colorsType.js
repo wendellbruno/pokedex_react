7612d125a517153bb6a6d66d01f471d6d5dd1b8e
wendellbruno <79750052+wendellbruno@users.noreply.github.com>
2023-06-12 17:13:08 -0300

    initial commit
    initial commit

---
+++ src/utils/colorsType.js
@@ -0,0 +1,22 @@
+const colorsTypes = {
+    
+    'Normal': 0xFFb3b5b2,
+    'Fire': 0xFFF4934D,
+    'Fighting': 0xFFEB4971,
+    'Water': 0xFF75b1e5,
+    'Flying': 0xFFB8A5F2,
+    'Grass': 0xFF7fca79,
+    'Poison': 0xFFc582d9,
+    'Electric': 0xFFF9DF78,
+    'Ground': 0xFFF78551,
+    'Psychic': 0xFFfb9d9a,
+    'Rock': 0xFFd4c9a1,
+    'Ice': 0xFF91d9cd,
+    'Bug': 0xFFB5C534,
+    'Dragon': 0xFF8656FA,
+    'Ghost': 0xFF7f8ac9,
+    'Dark': 0xFF7a7981,
+    'Steel ': 0xFF78aab5,
+    'Fairy': 0xFFf1a6eb,
+
+}
